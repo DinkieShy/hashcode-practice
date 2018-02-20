2d14e97a2be699456d66e869d6dc01e40946bae4
Dave Churchill <david.churchill72@gmail.com>
2018-02-20 20:15:03 +0000

    Added output to file

--- hashcode.cs
+++ hashcode.cs
@@ -1,5 +1,14 @@
-class main(){
+using System;
+using System.IO;
+
+class main{
 	static void Main(){
-		
+		string[] testText = {"Hi", "This is a test"};
+		outputToFile(testText);
+	}
+	
+	static void outputToFile(string[] textToOutput){ //save output to .txt file
+		string filename = "./output.txt";
+		File.WriteAllLines(filename, textToOutput);
 	}
 }
