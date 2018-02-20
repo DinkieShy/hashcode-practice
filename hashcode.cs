28e7523bf69092acf1abf1d49fa86b97063a2dde
izm0 <34348672+izm0@users.noreply.github.com>
2018-02-20 20:37:30 +0000

    Update hashcode.cs
    Added inputreader

--- hashcode.cs
+++ hashcode.cs
@@ -11,4 +11,19 @@ class main{
 		string filename = "./output.txt";
 		File.WriteAllLines(filename, textToOutput);
 	}
+	static void inputRead(){
+		string[] input = File.ReadAllLines("small.in");
+
+		int Rows,Cols,minIngred,maxCells;
+		Rows=Int32.Parse(input[0][0].ToString());
+		Cols=Int32.Parse(input[0][2].ToString());
+		minIngred=Int32.Parse(input[0][4].ToString());
+		maxCells=Int32.Parse(input[0][6].ToString());
+		Console.WriteLine(maxCells);
+		input[0] = "";
+
+		Console.ReadLine();
+		
+	}
+	
 }
